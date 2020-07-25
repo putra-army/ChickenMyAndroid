d55cafcb59c1594ed7ce691c9f5cdb3771bfe80a
putra-army <armhy333.mahasiswa.unikom.ac.id>
2020-07-25 14:40:05 +0700

    hilangkan tanda merah

--- app/src/main/java/com/example/chickenmy/Fragments/home/HomeFragment.java
+++ app/src/main/java/com/example/chickenmy/Fragments/home/HomeFragment.java
@@ -1,6 +1,5 @@
 package com.example.chickenmy.Fragments.home;
 
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,12 +9,10 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.chickenmy.Adapters.GridAdapter;
 import com.example.chickenmy.Databases.DashboardDatabase;
-import com.example.chickenmy.Databases.DatabaseInit;
 import com.example.chickenmy.Databases.Fix1S1Database;
 import com.example.chickenmy.Databases.PortableDatabase;
 import com.example.chickenmy.GetDate;
@@ -45,22 +42,22 @@ public class HomeFragment extends Fragment {
         mTime = root.findViewById(R.id.tv_home_welcome);
         mWellcome = root.findViewById(R.id.tv_home_hour);
 
-        tvSuhuKandang    = root.findViewById(R.id.tv_tertinggi_mati);
-        tvHumaKandang    = root.findViewById(R.id.tv_rerata_mati);
-        tvWindKandang    = root.findViewById(R.id.tv_wind_kandang);
-        tvAmoniaKandang  = root.findViewById(R.id.tv_terendah_mati);
+        tvSuhuKandang = root.findViewById(R.id.tv_tertinggi_mati);
+        tvHumaKandang = root.findViewById(R.id.tv_rerata_mati);
+        tvWindKandang = root.findViewById(R.id.tv_wind_kandang);
+        tvAmoniaKandang = root.findViewById(R.id.tv_terendah_mati);
 
-        tvUpdateTgl     = root.findViewById(R.id.tv_update_tgl);
-        tvUpdateTime    = root.findViewById(R.id.tv_update_time);
+        tvUpdateTgl = root.findViewById(R.id.tv_update_tgl);
+        tvUpdateTime = root.findViewById(R.id.tv_update_time);
 
-        tvHariKe        = root.findViewById(R.id.tv_hari_ke);
+        tvHariKe = root.findViewById(R.id.tv_hari_ke);
 
-        imgStsSuhu      = root.findViewById(R.id.imgStsSuhu);
-        imgStsHumi      = root.findViewById(R.id.imgStsHumi);
-        imgStsAmonia    = root.findViewById(R.id.imgStsAmonia);
-        imgStsAngin     = root.findViewById(R.id.imgStsAngin);
+        imgStsSuhu = root.findViewById(R.id.imgStsSuhu);
+        imgStsHumi = root.findViewById(R.id.imgStsHumi);
+        imgStsAmonia = root.findViewById(R.id.imgStsAmonia);
+        imgStsAngin = root.findViewById(R.id.imgStsAngin);
 
-        rvGrid  = root.findViewById(R.id.rincian_grid_recycler);
+        rvGrid = root.findViewById(R.id.rincian_grid_recycler);
 
         mTime.setText(getDate.getDateNow("E, dd MMMM yyyy"));
         hourNow = getDate.getTimeNow("HH");
@@ -85,83 +82,21 @@ public class HomeFragment extends Fragment {
 
                         MainActivity.db.setting.child("harike").setValue(jumlahHari);
 //
-                        tvSuhuKandang.setText(String.valueOf(temp) + "°C");
-//
-//                        // 1 - 3 Hari
-//                        if (jumlahHari < 3) {
-//                            if (Double.parseDouble(temp) < 29) {
-//                                imgStsSuhu.setImageDrawable(getResources().getDrawable(R.drawable.bulatbirusolid));
-//                            } else if (Double.parseDouble(temp) > 32) {
-//                                imgStsSuhu.setImageDrawable(getResources().getDrawable(R.drawable.bulatmerahsolid));
-//                            } else {
-//                                imgStsSuhu.setImageDrawable(getResources().getDrawable(R.drawable.bulatputihsolid));
-//                            }
-//                        }
-//
-//                        // 3 - 5
-//                        else if (jumlahHari < 6) {
-//                            if (Double.parseDouble(temp) < 28) {
-//                                imgStsSuhu.setImageDrawable(getResources().getDrawable(R.drawable.bulatbirusolid));
-//                            } else if (Double.parseDouble(temp) > 30) {
-//                                imgStsSuhu.setImageDrawable(getResources().getDrawable(R.drawable.bulatmerahsolid));
-//                            } else {
-//                                imgStsSuhu.setImageDrawable(getResources().getDrawable(R.drawable.bulatputihsolid));
-//                            }
-//                        }
-//
-//                        // 6 - 8
-//                        else if (jumlahHari < 9) {
-//                            if (Double.parseDouble(temp) < 25) {
-//                                imgStsSuhu.setImageDrawable(getResources().getDrawable(R.drawable.bulatbirusolid));
-//                            } else if (Double.parseDouble(temp) > 28) {
-//                                imgStsSuhu.setImageDrawable(getResources().getDrawable(R.drawable.bulatmerahsolid));
-//                            } else {
-//                                imgStsSuhu.setImageDrawable(getResources().getDrawable(R.drawable.bulatputihsolid));
-//                            }
-//                        }
-//
-//                        // 6 - 8
-//                        else if (jumlahHari <= 12) {
-//                            if (Double.parseDouble(temp) < 25) {
-//                                imgStsSuhu.setImageDrawable(getResources().getDrawable(R.drawable.bulatbirusolid));
-//                            } else if (Double.parseDouble(temp) > 28) {
-//                                imgStsSuhu.setImageDrawable(getResources().getDrawable(R.drawable.bulatmerahsolid));
-//                            } else {
-//                                imgStsSuhu.setImageDrawable(getResources().getDrawable(R.drawable.bulatputihsolid));
-//                            }
-//                        }
-//
-//                        // > 12
-//                        else {
-//                            if (Double.parseDouble(temp) < 24) {
-//                                imgStsSuhu.setImageDrawable(getResources().getDrawable(R.drawable.bulatbirusolid));
-//                            } else if (Double.parseDouble(temp) > 28) {
-//                                imgStsSuhu.setImageDrawable(getResources().getDrawable(R.drawable.bulatmerahsolid));
-//                            } else {
-//                                imgStsSuhu.setImageDrawable(getResources().getDrawable(R.drawable.bulatputihsolid));
-//                            }
-//                        }
+                        tvSuhuKandang.setText(temp + "°C");
+
+                        if (Double.parseDouble(temp) > 32) {
+                            imgStsSuhu.setImageDrawable(getResources().getDrawable(R.drawable.bulatmerahsolid));
+                        } else {
+                            imgStsSuhu.setImageDrawable(getResources().getDrawable(R.drawable.bulatputihsolid));
+                        }
                     }
                 });
 
-                tvHumaKandang.setText(String.valueOf(hum) + " %");
+                tvHumaKandang.setText(hum + " %");
                 tvUpdateTgl.setText(tgl);
                 tvUpdateTime.setText(time);
                 tvWindKandang.setText(wind + " m/s");
 
-//                //Kelembaban
-//                if (Double.parseDouble(hum) > 80) {
-//                    imgStsHumi.setImageDrawable(getResources().getDrawable(R.drawable.bulatmerahsolid));
-//                } else {
-//                    imgStsHumi.setImageDrawable(getResources().getDrawable(R.drawable.bulatputihsolid));
-//                }
-//
-//                //Kecepatan Angin
-//                if (Double.parseDouble(wind) > 1.47) {
-//                    imgStsAngin.setImageDrawable(getResources().getDrawable(R.drawable.bulatmerahsolid));
-//                } else {
-//                    imgStsAngin.setImageDrawable(getResources().getDrawable(R.drawable.bulatputihsolid));
-//                }
             }
 
         });
@@ -178,7 +113,7 @@ public class HomeFragment extends Fragment {
             public void AmoniaValue(Double amonia) {
                 DecimalFormat df = new DecimalFormat("#.##");
 
-                tvAmoniaKandang.setText(String.valueOf(df.format(amonia)) + " ppm");
+                tvAmoniaKandang.setText(df.format(amonia) + " ppm");
 
                 if (Double.valueOf(amonia) > 24) {
                     imgStsAmonia.setImageDrawable(getResources().getDrawable(R.drawable.bulatmerahsolid));
